7bd2fb237049994c6b619c236072140ab053b518
Alasdair Martin <martin_alasdair@yahoo.com>
2025-06-20 07:49:23 -0400

    Make a relative path on the load-templates file

--- js/load-templates.js
+++ js/load-templates.js
@@ -1,5 +1,5 @@
 // Load the header
-fetch('/templates/header.html')
+fetch('templates/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('site-header').innerHTML = data;
