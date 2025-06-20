02c37c974d7809ad44f70e6bf47e7a5c842e4383
Alasdair Martin <martin_alasdair@yahoo.com>
2025-06-20 07:37:34 -0400

    Add templating for header and nav

---
+++ js/load-templates.js
@@ -0,0 +1,6 @@
+// Load the header
+fetch('/templates/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('site-header').innerHTML = data;
+    });
