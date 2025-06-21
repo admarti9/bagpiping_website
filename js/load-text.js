784cc25b63e430a9d1ff66b30654ba932b4d8227
Alasdair Martin <martin_alasdair@yahoo.com>
2025-06-20 20:57:24 -0400

    Add welcome message

--- js/load-text.js
+++ js/load-text.js
@@ -4,3 +4,10 @@ fetch('content/about_text.txt')
     .then(data => {
         document.getElementById('about-text').innerText = data;
     });
+
+// Load welcome message
+fetch('content/welcome_message.txt')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('welcome_message').innerText = data;
+    });    
