68ed9f3736f41c710992b4cb8f879ca0b28beaca
Alasdair Martin <martin_alasdair@yahoo.com>
2025-06-20 20:35:14 -0400

    Templatized footer

--- js/load-templates.js
+++ js/load-templates.js
@@ -4,3 +4,10 @@ fetch('templates/header.html')
     .then(data => {
         document.getElementById('site-header').innerHTML = data;
     });
+
+// Load the footer
+fetch('templates/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('site-footer').innerHTML = data;
+    });    
